8ccb1bd7083db305e1320a57cd7a491d38c13f41
Leonardo Ventura <leonardo.ventura98@icloud.com>
2017-05-22 21:37:22 -0300

    Created new functions and started to code cleaner

--- game/breakout.c
+++ game/breakout.c
@@ -11,6 +11,9 @@
 #include <string.h>
 #include <time.h>
 
+#define BLOCK_WIDHT 120
+#define BLOCK_HEIGHT 90
+
 /*SDL_Surface* screen
 SDL_Window* window;
 SDL_Event event;*/
@@ -25,7 +28,7 @@ const int true = 1;
 const int false = 0;
 
 /* Defining structures */
-typedef struct _OBJECT {
+typedef struct _BALL {
   int posX;
   int posY;
   int stepX;
@@ -35,21 +38,29 @@ typedef struct _OBJECT {
   int imgH;
 } OBJECT;
 
+typedef struct _BLOCK {
+  int posX;
+  int posY;
+  int blockW;
+  int blockH;
+  int resistance;
+  SDL_Surface *image;
+} BLOCK;
+
 /* Global variables*/
 OBJECT ball;
-OBJECT bar;
 
 /* The window we'll be rendering to */
 SDL_Window* gWindow = NULL;
 
 /* Characters image */
-OBJECT barra;
+OBJECT bar;
 
 /* The surface contained by the window */
 SDL_Surface* gScreenSurface = NULL;
 
 /* Current displayed PNG image */
-SDL_Surface* gJPGSurface = NULL;
+SDL_Surface* gBallSurface = NULL;
 
 /* Functions prototypes */
 
@@ -71,6 +82,15 @@ OBJECT createOBJECT(int posX, int posY, int stepX, int stepY, SDL_Surface *image
 /* Move object function */
 void moveOBJECT(OBJECT *p);
 
+/* Create block function */
+BLOCK createBLOCK(int posX, int posY, SDL_Surface *image);
+
+/* function to draw blocks */
+void drawBlock(BLOCK b, SDL_Rect srcBlock);
+
+/* check collision between ball and block */
+BLOCK collision(BLOCK block, OBJECT *ball, int i);
+
 
 /* starts main function */
 
@@ -80,57 +100,108 @@ int main(int argc, char const *argv[]) {
   /* local variables */
   SDL_Rect srcRect, dstRect;
   int quit;
+  int i;
   /* event handler */
   SDL_Event e;
 
   /* Start up SDL and create window */
   if (!init())  {
-      printf("Failed to initialize SDL")
+      printf("SDL could not be initialized\n");
   }
 
-  else {
-    if (!loadMedia()) {
-      printf("Media could not be loaded\n");
+  if (!loadMedia()) {
+    printf("Media could not be loaded\n");
+  }
+  /* MAIN MENU CODE HERE */
+
+  /* create ball object */
+  ball = createOBJECT(SCREEN_WIDTH/2, (3*SCREEN_HEIGHT/4)+1), 0, 0, gJPGSurface);
+  /* create bar object */
+  bar = createOBJECT(SCREEN_WIDTH/2, (3*SCREEN_HEIGHT/4), 0, 0, gJPGSurface);
+
+  /* loop to create blocks */
+  for (i = 0; i < )
+
+  /* Starts game main loop */
+  while(SDL_PollEvent(&e) != 0) {
+    /* user request quit*/
+    if (e.type == SDL_Quit) {
+      quit = true;
     }
-
-    else {
-      /* MAIN MENU CODE HERE */
-
-      /* create ball object */
-      ball = createOBJECT(SCREEN_WIDTH/2, (3*SCREEN_HEIGHT/4)+1), 0, 0, gJPGSurface);
-      /* create bar object */
-      bar = createOBJECT(SCREEN_WIDTH/2, (3*SCREEN_HEIGHT/4), 0, 0, gJPGSurface);
-
-      /* Starts game main loop */
-      while(SDL_PollEvent(&e) != 0) {
-        /* user request quit*/
-        if (e.type == SDL_Quit) {
-          quit = true;
-        }
-        else if (e.type == SDL_KEYDOWN) {
-          switch(e.key.keysym.sym) {
-            /* user taps left arrow */
-            case SDLK_LEFT:
-              bar.posX += bar.stepX;
-              break;
-            /* user taps right arrow */
-            case SDLK_RIGHT:
-              bar.posX -= bar.stepX;
-              break;
-          }
-        }
+    else if (e.type == SDL_KEYDOWN) {
+      switch(e.key.keysym.sym) {
+        /* user taps left arrow */
+        case SDLK_LEFT:
+          bar.posX -= bar.stepX;
+          break;
+        /* user taps right arrow */
+        case SDLK_RIGHT:
+          bar.posX += bar.stepX;
+          break;
       }
 
+      /* Fill screen surface with white */
+      SDL_FillRect(gScreenSurface, NULL,
+                    SDL_MapRGB( gScreenSurface->format,
+                    0x00, 0x00, 0x00));
+
     }
   }
-
 
   return 0;
 }
 
 
+void drawBlock(BLOCK b, SDL_Rect srcBlock) {
+  SDL_Rect dstRect;
+  dstRect.x = b.posX;
+  dstRect.y = b.posY;
+  SDL_BlitSurface(b.image, &srcBlock, gScreenSurface, &dstRect);
+}
 
+BLOCK createBLOCK(int posX, int posY, int resistance, SDL_Surface *image) {
+  BLOCK b;
+  b.posX = posX;
+  b.posY = posY;
+  b.resistance = resistance;
+  b.image = image;
+  return b;
+}
 
+BLOCK collision(BLOCK block, NPC *ball, int i) {
+  /*left = ball.posX <= block.posX && ball.posY < block.posY + BLOCK_HEIGHT && ball.posY > block.posY;
+  right = ball.posX >= block.posX && ball.posY < block.posY + BLOCK_HEIGHT && ball.posY > block.posY;
+  down = ball.posX > block.posX && ball.posX < block.posX + BLOCK_WIDHT && ball.posY < block.posY + BLOCK_HEIGHT;
+  up = ball.posX > block.posX && ball.posX < block.posX + BLOCK_WIDHT && ball.posY < block.posY;
+  (i+1) to move the image
+  */
+
+  if (ball->posX > block.posX && ball->posX < block.posX + BLOCK_WIDHT && ball->posY < block.posY + BLOCK_HEIGHT) {
+    ball->stepY = -ball->stepY;
+    block.resistance -= 1;
+    block.posX = -BLOCK_WIDHT*(i+1);
+    block.posY = -BLOCK_HEIGHT*(i+1);
+  }
+  else if (ball->posX == block.posX && ball->posY < block.posY + BLOCK_HEIGHT && ball->posY > block.posY) {
+    ball->stepX = -ball->stepX;
+    block.resistance -= 1;
+    block.posX = -BLOCK_WIDHT*(i+1);
+    block.posY = -BLOCK_HEIGHT*(i+1);
+  }
+  else if (ball->posX == block.posX && ball->posY < block.posY + BLOCK_HEIGHT && ball->posY > block.posY) {
+    ball->stepX = -ball->stepX;
+    block.resistance -= 1;
+    block.posX = -BLOCK_WIDHT*(i+1);
+    block.posY = -BLOCK_HEIGHT*(i+1);
+  }
+  else if (ball.posX > block.posX && ball.posX < block.posX + BLOCK_WIDHT && ball.posY < block.posY) {
+    ball->stepY = -ball.stepY;
+    block.resistance -= 1;
+    block.posX = -BLOCK_WIDHT*(i+1);
+    block.posY = -BLOCK_HEIGHT*(i+1);
+  }
+  return block;
+}
 
 
 void moveOBJECT(OBJECT *p) {
@@ -202,9 +273,12 @@ int loadMedia() {
     int success = true;
     /*uint32_t colorKey;*/
 
-    /*Load PNG surface*/
-    gJPGSurface = loadSurface(/*"INSERT CHARACTER IMAGE PATH HERE"*/ );
-    if( gJPGSurface == NULL ) {
+    /*Load ball surface*/
+    gBallSurface = loadSurface(/*"INSERT BALL'S IMAGE PATH HERE"*/ );
+
+    /* load block surface */
+    gBlockSurface = loadSurface(/*INSERT BAR'S IMAGE PATH HERE */)
+    if( gBallSurface == NULL ) {
         printf( "Failed to load image! SDL Error: %s\n", SDL_GetError() );
         success = false;
     }
